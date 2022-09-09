1142485687659ebe58dcf828db365da0123576d1
Onyiiye <onyiyehelda14@gmail.com>
2022-09-09 01:15:12 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,31 +1,11 @@
-<<<<<<< HEAD
  #include <stdio.h>
 
 /**
-
-* main - main block
-
-* Return: 0
-
+* maain - main block
+* return: 0
 */
-
-int main(void)
-
-{
-
-​printf("with proper grammar, but the outcome is a piece of art,\n");
-
-​return (0);
-
-=======
-#include <stdio.h>
-/**
- * main - entry point
- * Return: 0
- */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
->>>>>>> 3bbed118ad331563a0bcb174938fd2e0f1f5a608
+	print("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }

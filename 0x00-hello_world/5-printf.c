2449d62ad276665b85f6b6e3ccd1a9bea848dd10
Onyiiye <onyiyehelda14@gmail.com>
2022-09-08 18:57:08 -0700

    Hello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-task 5
+
 #include <stdio.h>
 /**
  * main - entry point
@@ -6,6 +6,6 @@ task 5
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }

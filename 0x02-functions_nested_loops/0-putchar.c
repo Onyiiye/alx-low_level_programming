5d3539457abfd072798d6d08f58323dad757e17d
Onyiiye <onyiyehelda14@gmail.com>
2022-09-13 12:07:31 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,22 +1,20 @@
-#include "main.h"
+#includee "main.h"
 
 /**
-*main - prints the word Holberton
-*
-*Return: 0 on success execution.
-*/
+ * main - print putchar
+ * Return: alwaays 0
+ */
 
 int main(void)
 {
-	char s[] = "_putchar";
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar(10);
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
-}

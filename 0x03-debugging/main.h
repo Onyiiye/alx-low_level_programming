cf5d5e7e80dea66a24df2fccb63566fcd951f5b4
Onyiiye <onyiyehelda14@gmail.com>
2022-09-15 06:19:10 -0700

    print_remaining_days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,6 @@
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
-
-void convert_day(int, int);
+void convert_day(int, inf)
 
 #endif /* MAIN_H */

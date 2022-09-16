dbf311aca07af62937b94afd506cce843e104ad8
Onyiiye <onyiyehelda14@gmail.com>
2022-09-15 17:21:38 -0700

    print number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,60 +1,21 @@
-#include "main.h"
+include "main.h"
 
 /**
-*print_number - prints a numbe that uses _putchar
-*@n: number to print
-*Return: void
-*/
-
-void print_number(int n)
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
 {
-unsigned int un = 0;
-
-if (n < 0)
-{
-if (n < 1000000000)
-n = -n;
-un = n;
-_putchar(45);
-num_to_char(n);
-}
-else
-{
-un = n;
-num_to_char(un);
-}
-}
-
-/**
-*num_to_char - transforms a number with 1 or more digits into a char
-*@n: number to print
-*Return: void
-*/
-
-void num_to_char(unsigned int n)
-{
-unsigned int d = 10;
-
-if (n < d)
-{
-_putchar('0' + n);
-}
-else
-{
-while (n >= d)
-{
-d *= 10;
-if (d == 1000000000)
-break;
-}
-if (!(d == 1000000000) || n == 123456789)
-d /= 10;
-_putchar('0' + n / d);
-while (d != 10)
-{
-d /= 10;
-_putchar('0' + (n / d) % 10);
-}
-_putchar('0' + n % 10);
-}
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }

d1ae6b1a08f11976dea5f398f866c53535ef288b
Onyiiye <onyiyehelda14@gmail.com>
2022-09-26 05:18:27 -0700

    0-memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
- * _memset -fills memory with a constant byte
- * 
- * @s: pointer to string 
- * @b: constant byte
- * @n: first bytes to change
- * Return: pointer to modified string
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-    unsigned int i;
-
-    for (i = 0; i < n; i++
-	s[i] = b;
-    return (s);
-}
-
-#include "main.h"
-
-/**
  * _memset - fills memory with a constant byte
  *
  * @s: pointer to string
@@ -35,5 +16,3 @@ char *_memset(char *s, char b, unsigned int n)
 		s[i] = b;
 	return (s);
 }
-
-
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -15,6 +15,3 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
 #endif /* MAIN_H */
-
-
-

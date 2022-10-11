ecba23ddfe294804e46853f24da37cc0fb414eee
Onyiiye <onyiyehelda14@gmail.com>
2022-10-11 01:18:02 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,11 @@
-#include <stddef.h>
-#ifndef DOG_H
-#define DOG_H
+#ifndef STRTYP
+#define STRTYP
 
 /**
- * struct dog - contains dog information
- *
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- *
- * Description: hold all data needed for dog
+ * struct dog - structure of a god
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 struct dog
 {
@@ -17,13 +13,12 @@ struct dog
 	float age;
 	char *owner;
 };
-/**
- * dog_t - typedef for struct dog
- */
+
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-char *_strcpy(char *dest, char *src);
 void free_dog(dog_t *d);
-#endif /* DOG_H */
+
+#endif /* STRTYP */
